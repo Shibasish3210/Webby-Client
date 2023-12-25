ab87e57b65f35998e4a6729fe755ccbef19bb723
unknown <shibasish3210@gmail.com>
2023-12-25 21:30:18 +0530

    Server URL Updated

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const callApi = axios.create({
-    baseURL: 'https://webby-server.vercel.app',
+    baseURL: 'https://webby-server.onrender.com',
+    // baseURL: 'https://webby-server.vercel.app',
 })
 
 export default callApi
