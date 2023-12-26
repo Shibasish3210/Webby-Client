999161079c3748a6223a6e1413b4f8b56b5aef2c
unknown <shibasish3210@gmail.com>
2023-12-26 14:10:08 +0530

    vercel.json added

--- src/config/api.js
+++ src/config/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const callApi = axios.create({
     // baseURL: 'http://localhost:3333',
-    baseURL: 'https://webby-server.onrender.com',
-    // baseURL: 'https://webby-server.vercel.app',
+    // baseURL: 'https://webby-server.onrender.com',
+    baseURL: 'https://webby-server.vercel.app',
 })
 
 export default callApi
