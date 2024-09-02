aca036eda5dc95d9813f94de5721f85bc4630b86
Shibasish3210Gaian <shibhashish.m@gaiansolutions.com>
2024-09-02 16:01:03 +0530

    added clgs for debugging

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,64 +1,83 @@
-import { useRef } from 'react';
-import { Link, useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify';
-import { useDispatch } from 'react-redux';
-import Cookies from 'js-cookie';
-import { addUser } from '../reduxToolkit/slices/userSlice';
-import callApi from '../config/api';
-import Input from '../components/shared/Input';
-import Navbar from '../components/structured/Navbar';
-import Button from '../components/shared/Button';
-
-
-
+import { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import Cookies from "js-cookie";
+import { addUser } from "../reduxToolkit/slices/userSlice";
+import callApi from "../config/api";
+import Input from "../components/shared/Input";
+import Navbar from "../components/structured/Navbar";
+import Button from "../components/shared/Button";
 
 const Login = () => {
-  const loginId = useRef('');
-  const password = useRef('');
+	const loginId = useRef("");
+	const password = useRef("");
 
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
-  const handleLogin = async (e)=>{
-    e.preventDefault();
-    try {
-      const response = await callApi.post('/auth/login', {
-        loginId: loginId.current.value,
-        password: password.current.value
-      });
-      if(response.data.status !== 200){
-        toast.error(response.data.message);
-        return;
-      }
+	const handleLogin = async (e) => {
+		e.preventDefault();
+		console.log(
+			import.meta.env.VITE_ENVIRONMENT === "production"
+				? import.meta.env.VITE_devbaseURL
+				: import.meta.env.VITE_prodbaseURL,
+			import.meta.env.VITE_prodbaseURL,
+		);
+		try {
+			const response = await callApi.post("/auth/login", {
+				loginId: loginId.current.value,
+				password: password.current.value,
+			});
+			if (response.data.status !== 200) {
+				toast.error(response.data.message);
+				return;
+			}
 
-      const accessToken = response.data.accessToken;
-      
-      const user = response.data.user;
-      user.accessToken = accessToken; 
-      Cookies.set('USER_TOKEN', accessToken);
-      dispatch(addUser({...user}));
-      navigate('/dashboard');
-    } catch (error) {
-      toast.error(error.message);
-      console.log(error);
-    }
-  }
-  return (
-    <>
-    <Navbar/>
-    <form onSubmit={handleLogin}>
-      <div className="inputFields">
-        <Input id='loginId' label='User Name/ Email :' placeholder={'Enter your user name or email'} refrence={loginId}/>
-        <Input id='password' type={'password'} label='Your Password :' placeholder={'Enter your Password'} refrence={password}/>
-      </div>
+			const accessToken = response.data.accessToken;
 
-      <div className="mt-20 w-full text-center">
-        <Button type='submit' value={'Log In'}/>
-        <p className='mt-4'>{`Don't Have An Account?`} <Link className='pl-3' to={'/register'}>Click Here!</Link> </p>
-      </div>
-    </form>
-    </>
-  )
-}
+			const user = response.data.user;
+			user.accessToken = accessToken;
+			Cookies.set("USER_TOKEN", accessToken);
+			dispatch(addUser({ ...user }));
+			navigate("/dashboard");
+		} catch (error) {
+			toast.error(error.message);
+			console.log(error);
+		}
+	};
+	return (
+		<>
+			<Navbar />
+			<form onSubmit={handleLogin}>
+				<div className="inputFields">
+					<Input
+						id="loginId"
+						label="User Name/ Email :"
+						placeholder={"Enter your user name or email"}
+						refrence={loginId}
+					/>
+					<Input
+						id="password"
+						type={"password"}
+						label="Your Password :"
+						placeholder={"Enter your Password"}
+						refrence={password}
+					/>
+				</div>
 
-export default Login
+				<div className="mt-20 w-full text-center">
+					<Button type="submit" value={"Log In"} />
+					<p className="mt-4">
+						{`Don't Have An Account?`}{" "}
+						<Link className="pl-3" to={"/register"}>
+							Click Here!
+						</Link>{" "}
+					</p>
+				</div>
+			</form>
+		</>
+	);
+};
+
+export default Login;
